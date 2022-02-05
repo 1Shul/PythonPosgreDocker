7605b6418a10d0fbe8792337a47db61460643379
Shu <vicente.av.04@gmail.com>
2022-02-04 21:01:03 -0500

    Create tournaments feature (Update)

--- python/scripts/tournament.py
+++ python/scripts/tournament.py
@@ -2,18 +2,15 @@ from scripts.database import open_session
 from scripts.models import Tournament, Teams, Matches
 
 def create_tournament(tTitle):
-    session = open_session()
+    """
+    Function for tournament creation
+    """
 
-    # user=session.query(Tournament).all()
-    # print(user)
+    session = open_session()
 
     tournament=Tournament(title=tTitle)
     session.add(tournament)
 
-    # print('flush')
-
     session.commit()
-
-    # tour_tournament = session.query(Tournament).filter_by(title='Ptmr').first()
 
     session.close()
